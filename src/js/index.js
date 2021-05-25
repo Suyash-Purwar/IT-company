98a3e18a5725b39a905d93d4b23c30dbe4bd5449
suyash-purwar <purwarsuyash06@gmail.com>
2021-05-25 10:06:03 +0530

    Improved UX of navigation and made few more changes in nav

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,77 @@
-console.log("Successful test!!")
+const c_primary = '#226ce0';
+const c_grey = '#494949';
+const c_white = '#ffffff';
+const c_black = '#000000';
+const c_transparent = 'transparent';
+
+const nav = document.querySelector("#nav");
+const nav_logo = document.querySelector("#nav-logo");
+const nav_items = Array.from(document.querySelectorAll(".nav__item:not(:last-child)"));
+const nav_cta = document.querySelector(".nav__item:last-child");
+
+const sidenav_checkbox = document.querySelector("#navigation_checkbox");
+const sidenav_burger_bars = Array.from(document.querySelectorAll("[class^='sidenav__icon--']"));
+
+returnBarsArr = (removeMiddle) => {
+    if (removeMiddle) {
+        retArray = [];
+        for (i=0; i<sidenav_burger_bars.length; i++) {
+            if (i == 1) { continue }
+            retArray.push(sidenav_burger_bars[i]);
+        }
+
+        return retArray
+    } else {
+        return sidenav_burger_bars;
+    }
+}
+
+window.addEventListener('scroll', (e) => {
+    if (window.pageYOffset > 200) {
+        nav.style.backgroundColor = c_white;
+        nav_logo.style.fill = c_primary;
+        returnBarsArr(false).forEach(bar => {
+            bar.style.backgroundColor = c_grey;
+        });
+        nav_cta.style.border = `2px solid ${c_primary}`;
+        nav_items.forEach(item => {
+            item.style.color = c_grey;
+        });
+    } else {
+        nav.style.backgroundColor = c_transparent;
+        nav_logo.style.fill = c_white;
+        returnBarsArr(false).forEach(bar => {
+            bar.style.backgroundColor = c_white;
+        });
+        nav_cta.style.border = 'none';
+        nav_items.forEach(item => {
+            item.style.color = c_white;
+        });
+    }
+});
+
+sidenav_checkbox.addEventListener('change', (e) => {
+    if (sidenav_checkbox.checked) {
+        returnBarsArr(true).forEach(bar => {
+            bar.style.backgroundColor = c_grey;
+        });
+        returnBarsArr(false)[1].style.backgroundColor = c_transparent;
+        nav_logo.style.zIndex = '100';
+        if (window.pageYOffset < 200) {
+            nav_logo.style.fill = c_primary;
+            nav.style.backgroundColor = c_white;
+        }
+    } else {
+        if (window.pageYOffset < 200) {
+            returnBarsArr(false).forEach(bar => {
+                bar.style.backgroundColor = c_white;
+            });
+            nav_logo.style.fill = c_white;
+            nav.style.backgroundColor = c_transparent;
+        } else {
+            returnBarsArr(false).forEach(bar => {
+                bar.style.backgroundColor = c_grey;
+            });
+        }
+    }
+})
