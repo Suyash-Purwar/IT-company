ba71f7bfb41eb49819b772457843ac0be7f432f6
suyash-purwar <purwarsuyash06@gmail.com>
2021-06-16 19:19:33 +0530

    Position the bannerone component using pseudo elements

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ const watchTask = () => {
     watch('src/**/*.html', series(sendHtml, browsersyncReload));
     watch('src/sass/**/*.scss', series(compileSass, browsersyncReload));
     watch('src/js/*.js', series(jsmin, browsersyncReload));
-    watch('src/assets/**/*.{svg, png}', series(sendAssets, browsersyncReload));
+    watch('src/assets/**/*.{svg, png, svg}', series(sendAssets, browsersyncReload));
 }
 
 exports.default = series(
